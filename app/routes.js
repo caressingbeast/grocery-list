6faabcc5db3bf87a15fbb4a4c14427069dc12f97
Brandon Burning <bnburning3@gmail.com>
2015-09-01 14:03:21 -0700

    Update server routes

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,36 @@
 
 module.exports = function (app) {
 
+  // get all lists
+  app.get('/api/lists', function (req, res) {
+
+  });
+
+  // create a list
+  app.post('/api/list', function (req, res) {
+
+  });
+
+  // get a single list
+  app.get('/api/list/:list_id', function (req, res) {
+
+  });
+
+  // delete a list
+  app.delete('/api/list/:list_id', function (req, res) {
+
+  });
+
+  // create an item on a list
+  app.post('/api/list/:list_id/', function (req, res) {
+
+  });
+
+  // delete an item from a list
+  app.delete('/api/list/:list_id/:item_id', function (req, res) {
+
+  });
+
   app.get('*', function (req, res) {
     res.sendfile('./public/index.html');
   });
